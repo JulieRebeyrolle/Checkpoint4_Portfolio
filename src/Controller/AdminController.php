c7ca2affc94518ae169c59114e00359348278eeb
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2021-02-03 22:01:31 +0100

    created admin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin", name="admin_")
+ */
+
+class AdminController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
+
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+
+        ]);
+    }
+
+
+
+}
